126c182796fa1a89e84ea03af93eb410bad19572
gravvy23 <majkutw@gmail.com>
2020-05-16 16:26:55 +0200

    add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import { Navigation } from './components/Navigation';
 import { FunctionalWithState, FunctionalWithStateHook, SetCountCallback, FunctionalWithEffect } from  './examples';
+import { HomePage } from './components/HomePage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/state-hook" component={FunctionalWithStateHook} />
         <Route path="/setcount-callback" component={SetCountCallback} />
         <Route path="/use-effect" component={FunctionalWithEffect} />
+        <Route path="/" exact component={HomePage} />
       </Switch>
     </Router>
   );
